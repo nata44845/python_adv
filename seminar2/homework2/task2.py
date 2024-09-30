a48815647a4e2d6fa58a545628ea488865ce42a1
nata44845 <krppost@yandex.ru>
2024-09-30 15:25:51 +0700

    homework3

--- seminar2/homework2/task2.py
+++ seminar2/homework2/task2.py
@@ -2,3 +2,18 @@
 # Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
 # Функцию hex используйте для проверки своего результата.
 
+HEX = 16
+HEX_ARRAY = [i for i in range(10)] +['а','b','c','d','e','f']
+
+
+num = int(input("Введите число: "))
+x = num
+result: str = ''
+while x>0:
+    result = str(HEX_ARRAY[x%HEX])+result
+    x //= HEX
+print(f'{HEX=} {result=}')
+
+print(f'{hex(num)=}')
+
+
--- seminar2/homework2/task3.py
+++ seminar2/homework2/task3.py
@@ -2,3 +2,40 @@
 # Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
 # Программа должна возвращать сумму и произведение* дробей.
 # Для проверки своего кода используйте модуль fractions
+import fractions
+
+def short(n, m):
+    if n > m:
+        k = n
+    else:
+        k = m
+    while k != 1:
+        if n % k == 0 and m % k == 0:
+            return n // k, m // k
+        else:
+            k -= 1
+    return n, m
+
+
+num1 = input("Введите дробь 1: ")
+num2 = input("Введите дробь 2: ")
+
+c1, z1 = (int(i) for i in num1.split('/'))
+c2, z2 = (int(i) for i in num2.split('/'))
+
+csum = c1*z2+c2*z1
+zsum = z1*z2
+csum, zsum = short(csum, zsum)
+summa = str(csum)+'/'+str(zsum)
+
+cmult = c1*c2
+zmult = z1*z2
+cmult, zmult = short(cmult, zmult)
+mult= str(cmult)+'/'+str(zmult)
+
+print(f'Сумма {summa}\nПроизведение {mult}')
+
+print(f'Сумма {fractions.Fraction(c1,z1)+fractions.Fraction(c2,z2)}')
+print(f'Произведение {fractions.Fraction(c1,z1)*fractions.Fraction(c2,z2)}')
+
+

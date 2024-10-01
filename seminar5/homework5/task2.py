7c9d65cbf1a2bf7283c76c8c89ac9afec376c7ae
nata44845 <krppost@yandex.ru>
2024-10-01 12:49:59 +0700

    homework5

--- seminar5/homework5/task2.py
+++ seminar5/homework5/task2.py
@@ -1,3 +1,11 @@
 # Задание 2
 # Напишите функцию, которая принимает на вход строку - абсолютный путь до файла.
 # Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
+
+def parse_path(path):
+    path_only, _, file_name = path.rpartition('\\')
+    file_name, _, file_ext = file_name.rpartition(".")
+    return path_only, file_name, file_ext
+
+if __name__ == '__main__':
+    print(parse_path('C:\\Nata\\GeekBrains\\gb-git\\python_adv\\seminar3\\task1.py'))
--- seminar5/homework5/task4.py
+++ seminar5/homework5/task4.py
@@ -1,3 +1,14 @@
 # Задание 4
 # Создайте функцию генератор чисел Фибоначчи
 # https://ru.wikipedia.org/wiki/%D0%A7%D0%B8%D1%81%D0%BB%D0%B0_%D0%A4%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8
+
+def fib_generator(n: int):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+gen = fib_generator(20)
+
+for _ in range(10):
+    print(next(gen))
